2f9efa8450d9affd1174e5ef9111f6dca0c26579
AShigapov <ainur.shigapov@novardis.com>
2021-11-22 21:45:51 +0300

    added number comparing to CsvTokenService

--- src/main/java/com/example/csv_proj/svc/CsvTokenService.java
+++ src/main/java/com/example/csv_proj/svc/CsvTokenService.java
@@ -1,5 +1,6 @@
 package com.example.csv_proj.svc;
 
+import com.example.csv_proj.dto.CsvEntity;
 import com.example.csv_proj.dto.TokenEntity;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -10,6 +11,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,18 +19,26 @@ public class CsvTokenService {
 
     public void doWork() {
         File dbTokensFile = getFileFromURL("tokens.csv");
+        File mktFile = getFileFromURL("not-exist-numbers-in-mkt.csv");
+
         List<TokenEntity> tokenEntityList = null;
+        List<CsvEntity> mktObjList = null;
+
         try {
-            tokenEntityList = getListObj(dbTokensFile);
+            tokenEntityList = getTokenEntityListObj(dbTokensFile);
+            mktObjList = getListObj(mktFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         System.out.println(tokenEntityList.size());
 
+        Set<String> numbersSet = mktObjList.stream().map(CsvEntity::getMobNumber).collect(Collectors.toSet());
+
         tokenEntityList = tokenEntityList.stream()
                 .filter(item -> item.getMobNumber() != null && !Objects.equals(item.getMobNumber(), ""))
                 .distinct()
+                .filter(tokenEntity -> numbersSet.contains(tokenEntity.getMobNumber()))
                 .collect(Collectors.toList());
 
         System.out.println(tokenEntityList.size());
@@ -42,8 +52,7 @@ public class CsvTokenService {
 
     }
 
-
-    public void writeCsvFromBean(String pathName, List<TokenEntity> tokenEntityList) throws Exception {
+    private void writeCsvFromBean(String pathName, List<TokenEntity> tokenEntityList) throws Exception {
 
         if (tokenEntityList != null) {
             try (
@@ -66,10 +75,16 @@ public class CsvTokenService {
         System.out.println("FINISHED WORK");
     }
 
-    public List<TokenEntity> getListObj(File file) throws IOException {
+    private List<TokenEntity> getTokenEntityListObj(File file) throws IOException {
         CsvToBeanBuilder<TokenEntity> csvToBeanBuilder = new CsvToBeanBuilder<TokenEntity>(new FileReader(file));
         List<TokenEntity> tokenEntityList = csvToBeanBuilder.withType(TokenEntity.class).build().parse();
         return tokenEntityList;
+    }
+
+    public List<CsvEntity> getListObj(File file) throws IOException {
+        CsvToBeanBuilder<CsvEntity> csvToBeanBuilder = new CsvToBeanBuilder<CsvEntity>(new FileReader(file));
+        List<CsvEntity> paymentCSVEntities = csvToBeanBuilder.withType(CsvEntity.class).build().parse();
+        return paymentCSVEntities;
     }
 
     private File getFileFromURL(String fileName) {
